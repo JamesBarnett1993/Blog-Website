f774346af8b1c993f373b0657f521ad65b989787
JamesBarnett1993 <jb@trafalgar-international.com>
2015-04-01 21:10:30 +0100

    added image slider

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function() {
 
 		var $Navbar_li = $("#HeadbarNav li");
@@ -11,5 +13,38 @@ $(function() {
 		$(this).toggleClass('active'); 
 		
 		});
+
+		   //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 500;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
 		
 	});
