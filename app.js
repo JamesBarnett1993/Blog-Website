89ed91721a949de5e7a5832abcfe31fa30bf5361
JamesBarnett1993 <jamesbarnett1993@icloud.com>
2016-06-28 10:42:20 +0100

    changed title and animation settings

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ $(function() {
 
 		   //settings for slider
     var width = 720;
-    var animationSpeed = 1000;
-    var pause = 500;
+    var animationSpeed = 700;
+    var pause = 1000;
     var currentSlide = 1;
 
     //cache DOM elements
